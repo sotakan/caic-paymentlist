723ac85646f30265cfd67f252dde5dfb85048031
Sota Kaneko <me@sotakan.com>
2022-11-21 05:00:55 +0900

    setup auth flow for testing

--- main.py
+++ main.py
@@ -11,37 +11,40 @@ from googleapiclient.errors import HttpError
 import os
 
 # Filters by date and tx type
+
+
 def import_syncable(syncablecsv, year):
-    raw = pd.read_csv(syncablecsv, parse_dates = ["寄付日"])
+    raw = pd.read_csv(syncablecsv, parse_dates=["寄付日"])
 
     # Select rows with matching start date and payment is a subscription
-    start_matching = raw.loc[(raw["寄付日"] >= dt(year,1,1)) & (raw["種別"] == "年会費")]
+    start_matching = raw.loc[(raw["寄付日"] >= dt(
+        year, 1, 1)) & (raw["種別"] == "年会費")]
 
     # Return rows with end date
-    return start_matching.loc[(start_matching["寄付日"] < dt(year+1,1,1)) & (start_matching["種別"] == "年会費")]
+    return start_matching.loc[(start_matching["寄付日"] < dt(year+1, 1, 1)) & (start_matching["種別"] == "年会費")]
 
 # Filters by tx deposit
-def import_sbi(sbicsv):
-    raw = pd.read_csv(sbicsv, parse_dates = ["日付"], encoding = "shift-jis")
 
-    deposits =raw.dropna(subset = "入金金額(円)")
+
+def import_sbi(sbicsv):
+    raw = pd.read_csv(sbicsv, parse_dates=["日付"], encoding="shift-jis")
+
+    deposits = raw.dropna(subset="入金金額(円)")
 
     return deposits.fillna(0)
 
-# Get sheeets member info
-def import_members():
+# Authenticate Google API
+def auth_google():
     scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
     try:
-        spreadsheetid = os.environ["CAIC_PAYMENTLIST_SHEETID"]
-        sheetrange = os.environ["CAIC_PAYMENTLIST_SHEETRANGE"]
         credpath = os.environ["CAIC_PAYMENTLIST_CREDPATH"]
     except KeyError:
         raise Exception("Environment variables not set")
 
     creds = None
     if os.path.exists(f'{credpath}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'{credpath}/token.json', scopes)
+        creds = Credentials.from_authorized_user_file(
+            f'{credpath}/token.json', scopes)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -54,9 +57,20 @@ def import_members():
         with open(f'{credpath}/token.json', 'w') as token:
             token.write(creds.to_json())
 
+    return creds
+
+
+# Get sheeets member info
+def import_members(creds):
+    try:
+        spreadsheetid = os.environ["CAIC_PAYMENTLIST_SHEETID"]
+        sheetrange = os.environ["CAIC_PAYMENTLIST_SHEETRANGE"]
+    except KeyError:
+        raise Exception("Environment variables not set")
+
     service = build("sheets", "v4", credentials=creds)
     sheets = service.spreadsheets()
 
     ret = sheets.values().get(spreadsheetId=spreadsheetid, range=sheetrange).execute()
 
-    return ret.get("values",[])
+    return ret.get("values", [])
--- test/test_main.py
+++ test/test_main.py
@@ -5,11 +5,14 @@ import pandas as pd
 import main
 import os
 
+from google.oauth2.credentials import Credentials
+
+
 class testMain(unittest.TestCase):
     def testImport_Syncable(self):
         exp = {
-            "id": [2,4,5],
-            "寄付日": [pd.Timestamp(2021,1,27,14,30), pd.Timestamp(2021,1,1,14,30), pd.Timestamp(2021,12,31,14,30)],
+            "id": [2, 4, 5],
+            "寄付日": [pd.Timestamp(2021, 1, 27, 14, 30), pd.Timestamp(2021, 1, 1, 14, 30), pd.Timestamp(2021, 12, 31, 14, 30)],
             "金額": [12000, 12000, 12000],
             "手数料": [660, 660, 660],
             "種別": ["年会費", "年会費", "年会費"],
@@ -23,12 +26,12 @@ class testMain(unittest.TestCase):
 
     def testImport_sbi(self):
         exp = {
-            "日付": [pd.Timestamp(2022,10,21), pd.Timestamp(2022,10,16), pd.Timestamp(2022,10,12)],
+            "日付": [pd.Timestamp(2022, 10, 21), pd.Timestamp(2022, 10, 16), pd.Timestamp(2022, 10, 12)],
             "内容": ["振込＊テスト　タロウジュニア", "利息", "振込＊テスト　タロウシニア"],
             "出金金額(円)": [0, 0, 0],
             "入金金額(円)": ["17,000", "2", "17,000"],
             "残高(円)": ["3,018,066", "3,001,066", "3,001,064"],
-            "メモ": ["-","-","-"]}
+            "メモ": ["-", "-", "-"]}
 
         raw = main.import_sbi("test/testsbi.csv")
         ret = raw.to_dict("list")
@@ -36,11 +39,17 @@ class testMain(unittest.TestCase):
         self.assertEqual(ret, exp)
 
     def testImport_member(self):
-        os.environ["CAIC_PAYMENTLIST_SHEETID"] = "1TkDu5TI5T7aPuhVVGAF4XhBtGEDEPVspmqkSJSv-bkI"
-        os.environ["CAIC_PAYMENTLIST_SHEETRANGE"] = "会費確認!A:C"
-        os.environ["CAIC_PAYMENTLIST_CREDPATH"] = "creds"
 
-        exp = ["会員種別", "氏名", "口座名義"]
-        ret = main.import_members()
+        if os.environ.get("CI"):
+            creds = Credentials(token=os.environ.get("CI_TOKEN"), refresh_token=os.environ.get("CI_REFRESH_TOKEN"), token_uri=os.environ.get("CI_TOKEN_URI"), client_id=os.environ.get("CI_CLIENT_ID"), client_secret=os.environ.get("CI_CLIENT_SECRET"), scopes= [os.environ.get("CI_SCOPES")])
+        else:
+            os.environ["CAIC_PAYMENTLIST_CREDPATH"]="creds"
+            creds = main.auth_google()
+
+        os.environ["CAIC_PAYMENTLIST_SHEETID"]="1TkDu5TI5T7aPuhVVGAF4XhBtGEDEPVspmqkSJSv-bkI"
+        os.environ["CAIC_PAYMENTLIST_SHEETRANGE"]="会費確認!A:C"
+        
+        exp=["会員種別", "氏名", "口座名義"]
+        ret=main.import_members(creds)
 
         self.assertEqual(exp, ret[0])
